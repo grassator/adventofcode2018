d601f04803db94481ce264e8e6c2e657e0124435
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2018-12-03 20:32:36 +0100

    Initial Setup

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import qualified Day1
+
+main :: IO ()
+main = putStr ( show ( Day1.calibrate "" ) )
---
+++ src/Day1.hs
@@ -0,0 +1,8 @@
+module Day1
+    ( calibrate
+    ) where
+
+import Data.Text as Text
+
+calibrate :: Text -> Int
+calibrate _ = 0
