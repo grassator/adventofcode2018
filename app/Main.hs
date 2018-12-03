9eea3be9dc6b7e60ee6710c5e53f32d7b6c91451
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2018-12-03 21:37:34 +0100

    Day 1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import qualified Day1
+import qualified Data.Text.IO as TIO
+import System.Environment (getArgs)
 
 main :: IO ()
-main = putStr ( show ( Day1.calibrate "" ) )
+main = do
+    args <- getArgs
+    case args of
+        [] -> do
+            putStrLn "Please select a day, e.g:"
+            putStrLn "  adventofcode 1"
+        "1":_ -> do
+            text <- TIO.readFile "./data/day1.txt"
+            putStrLn $ "1: " ++ ( show ( Day1.calibrate text ) )
+            putStrLn $ "2: " ++ ( show ( Day1.findRepeating text ) )
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,8 +1,31 @@
 module Day1
     ( calibrate
+    , findRepeating
     ) where
 
-import Data.Text as Text
+import qualified Data.Text as T
+import qualified Data.Set as Set
 
-calibrate :: Text -> Int
-calibrate _ = 0
+parse :: T.Text -> [Int]
+parse input = 
+    let
+        parseInt = ( read . T.unpack . (T.replace "+" "") )
+    in
+        parseInt <$> ( T.splitOn "\n" input )
+
+calibrate :: T.Text -> Int
+calibrate = ( foldl (+) 0 ) . parse
+
+findRepeating :: T.Text -> Int
+findRepeating =
+    let
+        find prev seen (el:rest) =
+            let
+                current = prev + el
+                found = Set.member current seen
+            in
+                if found
+                then current
+                else find current ( Set.insert current seen ) rest
+    in
+        ( find 0 Set.empty ) . cycle . parse
