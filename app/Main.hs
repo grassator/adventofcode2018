37162d222f2774059e46654e6a01649a7c24438a
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2018-12-11 21:16:36 +0100

    Refactor main runner

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,24 @@ module Main where
 import qualified Day1
 import qualified Day2
 import qualified Day3
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
+
+
+run :: (Show a, Show b) => String -> (T.Text -> a) -> (T.Text -> b) -> IO ()
+run day first second = do
+    text <- TIO.readFile $ "./data/day" ++ day ++ ".txt"
+    putStrLn $ "1: " ++ ( show ( first text ) )
+    putStrLn $ "2: " ++ ( show ( second text ) )
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [] -> do
+        "1":_ -> run "1" Day1.calibrate Day1.findRepeating
+        "2":_ -> run "2" Day2.checksum Day2.offByOne
+        "3":_ -> run "3" Day3.countOverlapping Day3.findNonOverlapping
+        _ -> do
             putStrLn "Please select a day, e.g:"
             putStrLn "  adventofcode 1"
-        "1":_ -> do
-            text <- TIO.readFile "./data/day1.txt"
-            putStrLn $ "1: " ++ ( show ( Day1.calibrate text ) )
-            putStrLn $ "2: " ++ ( show ( Day1.findRepeating text ) )
-        "2":_ -> do
-            text <- TIO.readFile "./data/day2.txt"
-            putStrLn $ "1: " ++ ( show ( Day2.checksum text ) )
-            putStrLn $ "2: " ++ ( show ( Day2.offByOne text ) )
-        "3":_ -> do
-            text <- TIO.readFile "./data/day3.txt"
-            putStrLn $ "1: " ++ ( show ( Day3.countOverlapping text ) )
-            putStrLn $ "2: " ++ ( show ( Day3.findNonOverlapping text ) )
